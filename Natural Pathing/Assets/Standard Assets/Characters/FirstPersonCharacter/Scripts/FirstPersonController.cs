fb5152fc799c61d32ac85556626a96cb0cb94014
mplecapt <mplecapt@mtu.edu>
2016-10-04 13:33:29 -0400

    Added AI
    Added an AI character that moves to point b using navmesh.

--- Natural Pathing/Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/FirstPersonController.cs
+++ Natural Pathing/Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/FirstPersonController.cs
@@ -63,7 +63,7 @@ namespace UnityStandardAssets.Characters.FirstPerson
         {
             RotateView();
             // the jump state needs to read here to make sure it is not missed
-            if (!m_Jump && m_CharacterController.isGrounded)
+            if (!m_Jump)
             {
                 m_Jump = CrossPlatformInputManager.GetButtonDown("Jump");
             }
